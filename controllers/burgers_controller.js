5ad26a8970bb2829e303bd637d6b35a8f73410f5
ShemikaKW <Shemikakw@gmail.com>
2018-11-18 09:15:27 -0500

    complete

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,7 +5,41 @@ var router = express.Router();
 var burger = require('../models/burger.js');
 
 //Create all routes and set up logic within the routes
+router.get ('/', function(req, res) {
+    burger.all(function(data) {
+        var hbsOjbect = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render('index', hbsOjbect);
+    });
+});
 
+router.post('/burger/create', function(req, res) {
+    burger.create(req.body.burgers, function () {
+       res.status(200).end(); 
+    });
+});
+
+router.put('/burger/update/:id', function(req, res) {
+    burger.update(req.body.devoured, req.params.id, function () {
+        res.status(200).end(); 
+    });
+});
+
+router.delete('/burger/delete/:id', function(req, res) {
+    var condition = 'id = ' + req.params.id;
+
+    burger.delete(condition, function() {
+        res.redirect('/');
+    });
+
+router.put('/burger/update/:id', function(req, res) {
+    burger.update(req.body.create, req.params.id, function () {
+        res.status(200).end();
+    })
+});
+});
 
 //Exports routes for server.js to use
 module.exports = router;
